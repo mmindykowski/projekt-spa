d78e5d4e6eea0f3c366b2bbdeed316db0278b97d
mmindykowski <128606230+mmindykowski@users.noreply.github.com>
2023-06-07 22:06:18 +0200

    obsługa formularza

--- js/main.js
+++ js/main.js
@@ -1,7 +1,37 @@
-document.getElementsByClassName('mobile-hamburger')[0].addEventListener('click', function() {
-    document.getElementsByClassName('open-menu-holder')[0].classList.toggle('open');
-});
+document
+  .getElementsByClassName("mobile-hamburger")[0]
+  .addEventListener("click", function () {
+    document
+      .getElementsByClassName("open-menu-holder")[0]
+      .classList.toggle("open");
+  });
 
-document.getElementsByClassName('mobile-close')[0].addEventListener('click', function() {
-    document.getElementsByClassName('open-menu-holder')[0].classList.toggle('open');
-});
+document
+  .getElementsByClassName("mobile-close")[0]
+  .addEventListener("click", function () {
+    document
+      .getElementsByClassName("open-menu-holder")[0]
+      .classList.toggle("open");
+  });
+
+document
+  .getElementById("appointment-form")
+  .addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const appointmentMessage = document.querySelector(".appointment-message");
+    let formFields = document.getElementsByClassName("form-field");
+    let allFields = false;
+
+    for (let i = 0; i < formFields.length; i++) {
+      if (formFields[i].value === "") {
+        allFields = false;
+        formFields[i].classList.add("error");
+      } else {
+        allFields = true;
+        formFields[i].classList.remove("error");
+      }
+    }
+
+    console.log("wyslanie fotm");
+  });
