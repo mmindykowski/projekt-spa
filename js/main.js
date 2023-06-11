b293ed57551c85d2128785e0e7b4411ac26ad795
mmindykowski <128606230+mmindykowski@users.noreply.github.com>
2023-06-11 21:44:37 +0200

    poprawienie bledu w kodzie js - bledne id

--- js/main.js
+++ js/main.js
@@ -23,14 +23,14 @@ document
     let formFields = document.getElementsByClassName("form-field");
     let allFields = false;
     let appointment = {
-        name: document.getElementById('appointment-name').value,
-        email: document.getElementById('appointment-email').value,
-        service: document.getElementById('appointment-service'),
-        phone: document.getElementById('appointment-phone').value,
-        date: document.getElementById('appointment-date').value,
-        time: document.getElementById('appointment-time').value,
-        message: document.getElementById('appointment-message').value
-    }
+      name: document.getElementById("appointment-name").value,
+      email: document.getElementById("appointment-email").value,
+      service: document.getElementById("appointment-select").value,
+      phone: document.getElementById("appointment-phone").value,
+      date: document.getElementById("appointment-date").value,
+      time: document.getElementById("appointment-time").value,
+      message: document.getElementById("appointment-message").value,
+    };
 
     for (let i = 0; i < formFields.length; i++) {
       if (formFields[i].value === "") {
@@ -45,7 +45,7 @@ document
     if (allFields) {
       console.log(appointment);
     } else {
-        appointmentMessage.classList.add('error');
-        appointmentMessage.innerText = `Wypełnij wymagane pola!`
+      appointmentMessage.classList.add("error");
+      appointmentMessage.innerText = `Wypełnij wymagane pola!`;
     }
   });
